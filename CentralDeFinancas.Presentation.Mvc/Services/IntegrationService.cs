7f759d60a033455563b375d7c5b4a67cf612871f
LucasMMF <mateusmidao@gmail.com>
2023-02-05 12:14:35 -0300

    Consumo da API Central de Usuários, integração com SignalR.

---
+++ CentralDeFinancas.Presentation.Mvc/Services/IntegrationService.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace CentralDeFinancas.Presentation.Mvc.Services
+{
+    public class IntegrationService
+    {
+        private readonly string? _apiUrl;
+
+        public IntegrationService(string? apiUrl)
+        {
+            _apiUrl = apiUrl;
+        }
+
+        public async Task<string> PostAsync(string endpoint, StringContent content)
+        {
+            using (var HttpClient = new HttpClient())
+            {
+                // Requisição POST para a API
+                var result = await HttpClient.PostAsync(_apiUrl + endpoint, content);
+
+                // Lendo a resposta da requisição
+                var builder = new StringBuilder();
+                using (var response = result.Content)
+                {
+                    Task<string> task = response.ReadAsStringAsync();
+                    builder.Append(task.Result);
+                }
+
+                var jsonResult = builder.ToString();
+
+                // Verificando se a resposta é sucesso
+                if (result.IsSuccessStatusCode)
+                    return jsonResult;
+                else
+                    throw new Exception(jsonResult);
+            }
+        }
+    }
+}
